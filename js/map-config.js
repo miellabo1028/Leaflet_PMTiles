336cefda8bcfe117c217ee916ad63121a647d115
miellabo1028 <68525651+miellabo1028@users.noreply.github.com>
2026-09-25 15:12:15 +0900

    Update map-config.js

--- js/map-config.js
+++ js/map-config.js
@@ -23,7 +23,8 @@ const GESAT_CONFIG = {
     
     // Single band data saumple through TiTiler
     // mapBiomas2024: "http://localhost:8000/cog/tiles/WebMercatorQuad/{z}/{x}/{y}.png" + "?url=file:///data/MapBiomas/Bol_Mapbiomas_LULC_2024_cog.tif",
-    mapBiomas2024: "http://localhost:8000/cog/tiles/WebMercatorQuad/{z}/{x}/{y}" + "?url=file:///data/MapBiomas/Bol_Mapbiomas_LULC_2024_cog.tif" + "&colormap_name=viridis" + "&tilesize=512",
+    // mapBiomas2024: "http://localhost:8000/cog/tiles/WebMercatorQuad/{z}/{x}/{y}" + "?url=file:///data/MapBiomas/Bol_Mapbiomas_LULC_2024_cog.tif" + "&colormap_name=viridis" + "&tilesize=512",
+    mapBiomasCog: "file:///data/MapBiomas/Bol_Mapbiomas_LULC_2024_cog.tif",
     
     // 今回のボリビア市町村FlatGeobufを追加
     boliviaMunicipiosFgb: "https://pub-d44086fa57624870a3a45a6fd01ea211.r2.dev/bolivia_municipios.fgb"
